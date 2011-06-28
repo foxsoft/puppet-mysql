856d52e9e94bd1e4168e821733659ada93a3d3aa
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-06-28 22:12:11 +0100

    Ensure MySQL running

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,13 @@
 class mysql {
   package { 'mysql': ensure => installed }
+  
+  Service["mysql"] {
+    start   => "/sbin/service mysql start",
+    status  => "/sbin/service mysql status",
+    stop    => "/sbin/service mysql stop",
+    restart => "/sbin/service mysql restart",
+    require => [Package["mysql"],
+  }
+  
+  service {"mysql": ensure => running, require => Package["mysql"] }
 }
