89787ef66387edaff5ee17daf8da2e0718c68bc0
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-06-28 22:21:20 +0100

    Also install server / devel, service should apply to server

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,14 @@
 class mysql {
   package { 'mysql': ensure => installed }
+  package { 'mysql-server': ensure => installed }
+  package { 'mysql-devel': ensure => installed }
   
   Service["mysql"] {
-    start   => "/sbin/service mysql start",
-    status  => "/sbin/service mysql status",
-    stop    => "/sbin/service mysql stop",
-    restart => "/sbin/service mysql restart",
-    require => Package["mysql"]
+    start   => "/sbin/service mysqld start",
+    status  => "/sbin/service mysqld status",
+    stop    => "/sbin/service mysqld stop",
+    restart => "/sbin/service mysqld restart",
+    require => Package["mysql-server"]
   }
   
   service {"mysql": ensure => running }
